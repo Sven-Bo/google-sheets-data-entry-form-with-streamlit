747371073d72a3c7278e9d67a0affb9ac1383909
Sven Bosau <sven@pythonandvba.com>
2023-10-28 19:27:08 +0200

    Added 'DELETE' functionality and option to view the entire dataframe. Closes #1.

--- vendor_app.py
+++ vendor_app.py
@@ -28,13 +28,23 @@ conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 existing_data = conn.read(worksheet="Vendors", usecols=list(range(6)), ttl=5)
 existing_data = existing_data.dropna(how="all")
 
-action = st.selectbox("Choose an Action", ["Onboard New Vendor", "Update Existing Vendor"])
+action = st.selectbox(
+    "Choose an Action",
+    [
+        "Onboard New Vendor",
+        "Update Existing Vendor",
+        "View All Vendors",
+        "Delete Vendor",
+    ],
+)
 
 if action == "Onboard New Vendor":
     st.markdown("Enter the details of the new vendor below.")
     with st.form(key="vendor_form"):
         company_name = st.text_input(label="Company Name*")
-        business_type = st.selectbox("Business Type*", options=BUSINESS_TYPES, index=None)
+        business_type = st.selectbox(
+            "Business Type*", options=BUSINESS_TYPES, index=None
+        )
         products = st.multiselect("Products Offered", options=PRODUCTS)
         years_in_business = st.slider("Years in Business", 0, 50, 5)
         onboarding_date = st.date_input(label="Onboarding Date")
@@ -49,16 +59,18 @@ if action == "Onboard New Vendor":
             elif existing_data["CompanyName"].str.contains(company_name).any():
                 st.warning("A vendor with this company name already exists.")
             else:
-                vendor_data = pd.DataFrame([
-                    {
-                        "CompanyName": company_name,
-                        "BusinessType": business_type,
-                        "Products": ", ".join(products),
-                        "YearsInBusiness": years_in_business,
-                        "OnboardingDate": onboarding_date.strftime("%Y-%m-%d"),
-                        "AdditionalInfo": additional_info,
-                    }
-                ])
+                vendor_data = pd.DataFrame(
+                    [
+                        {
+                            "CompanyName": company_name,
+                            "BusinessType": business_type,
+                            "Products": ", ".join(products),
+                            "YearsInBusiness": years_in_business,
+                            "OnboardingDate": onboarding_date.strftime("%Y-%m-%d"),
+                            "AdditionalInfo": additional_info,
+                        }
+                    ]
+                )
                 updated_df = pd.concat([existing_data, vendor_data], ignore_index=True)
                 conn.update(worksheet="Vendors", data=updated_df)
                 st.success("Vendor details successfully submitted!")
@@ -66,16 +78,36 @@ if action == "Onboard New Vendor":
 elif action == "Update Existing Vendor":
     st.markdown("Select a vendor and update their details.")
 
-    vendor_to_update = st.selectbox("Select a Vendor to Update", options=existing_data["CompanyName"].tolist())
-    vendor_data = existing_data[existing_data["CompanyName"] == vendor_to_update].iloc[0]
+    vendor_to_update = st.selectbox(
+        "Select a Vendor to Update", options=existing_data["CompanyName"].tolist()
+    )
+    vendor_data = existing_data[existing_data["CompanyName"] == vendor_to_update].iloc[
+        0
+    ]
 
     with st.form(key="update_form"):
-        company_name = st.text_input(label="Company Name*", value=vendor_data["CompanyName"])
-        business_type = st.selectbox("Business Type*", options=BUSINESS_TYPES, index=BUSINESS_TYPES.index(vendor_data["BusinessType"]))
-        products = st.multiselect("Products Offered", options=PRODUCTS, default=vendor_data["Products"].split(", "))
-        years_in_business = st.slider("Years in Business", 0, 50, int(vendor_data["YearsInBusiness"]))
-        onboarding_date = st.date_input(label="Onboarding Date", value=pd.to_datetime(vendor_data["OnboardingDate"]))
-        additional_info = st.text_area(label="Additional Notes", value=vendor_data["AdditionalInfo"])
+        company_name = st.text_input(
+            label="Company Name*", value=vendor_data["CompanyName"]
+        )
+        business_type = st.selectbox(
+            "Business Type*",
+            options=BUSINESS_TYPES,
+            index=BUSINESS_TYPES.index(vendor_data["BusinessType"]),
+        )
+        products = st.multiselect(
+            "Products Offered",
+            options=PRODUCTS,
+            default=vendor_data["Products"].split(", "),
+        )
+        years_in_business = st.slider(
+            "Years in Business", 0, 50, int(vendor_data["YearsInBusiness"])
+        )
+        onboarding_date = st.date_input(
+            label="Onboarding Date", value=pd.to_datetime(vendor_data["OnboardingDate"])
+        )
+        additional_info = st.text_area(
+            label="Additional Notes", value=vendor_data["AdditionalInfo"]
+        )
 
         st.markdown("**required*")
         update_button = st.form_submit_button(label="Update Vendor Details")
@@ -85,19 +117,46 @@ elif action == "Update Existing Vendor":
                 st.warning("Ensure all mandatory fields are filled.")
             else:
                 # Removing old entry
-                existing_data.drop(existing_data[existing_data["CompanyName"] == vendor_to_update].index, inplace=True)
+                existing_data.drop(
+                    existing_data[
+                        existing_data["CompanyName"] == vendor_to_update
+                    ].index,
+                    inplace=True,
+                )
                 # Creating updated data entry
-                updated_vendor_data = pd.DataFrame([
-                    {
-                        "CompanyName": company_name,
-                        "BusinessType": business_type,
-                        "Products": ", ".join(products),
-                        "YearsInBusiness": years_in_business,
-                        "OnboardingDate": onboarding_date.strftime("%Y-%m-%d"),
-                        "AdditionalInfo": additional_info,
-                    }
-                ])
+                updated_vendor_data = pd.DataFrame(
+                    [
+                        {
+                            "CompanyName": company_name,
+                            "BusinessType": business_type,
+                            "Products": ", ".join(products),
+                            "YearsInBusiness": years_in_business,
+                            "OnboardingDate": onboarding_date.strftime("%Y-%m-%d"),
+                            "AdditionalInfo": additional_info,
+                        }
+                    ]
+                )
                 # Adding updated data to the dataframe
-                updated_df = pd.concat([existing_data, updated_vendor_data], ignore_index=True)
+                updated_df = pd.concat(
+                    [existing_data, updated_vendor_data], ignore_index=True
+                )
                 conn.update(worksheet="Vendors", data=updated_df)
                 st.success("Vendor details successfully updated!")
+
+# View All Vendors
+elif action == "View All Vendors":
+    st.dataframe(existing_data)
+
+# Delete Vendor
+elif action == "Delete Vendor":
+    vendor_to_delete = st.selectbox(
+        "Select a Vendor to Delete", options=existing_data["CompanyName"].tolist()
+    )
+
+    if st.button("Delete"):
+        existing_data.drop(
+            existing_data[existing_data["CompanyName"] == vendor_to_delete].index,
+            inplace=True,
+        )
+        conn.update(worksheet="Vendors", data=existing_data)
+        st.success("Vendor successfully deleted!")
